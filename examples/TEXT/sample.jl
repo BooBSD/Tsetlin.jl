4927a55289e1c7830ed0fdd5babbb69c785a6771
Artem <boobsd@gmail.com>
2026-01-07 12:54:42 +0100

    Simplified Shakespeare text generating a bit

--- examples/TEXT/sample.jl
+++ examples/TEXT/sample.jl
@@ -8,27 +8,23 @@ using .Tsetlin: TMInput, TMClassifier, predict, load
 
 
 hvectors = deserialize(HV_PATH)
+tm = load(TM_PATH)
 
-function sample()
-    tm = load(TM_PATH)
-    prompt = PROMPT[max(end - CONTEXT_SIZE + 1, 1):end]
-    prompt_vector = [UInt8(t) for t in prompt]
-    for n in 1:TOKENS_GENERATE
-        con = @view(prompt_vector[max(end - CONTEXT_SIZE + 1, 1):end])
+prompt = PROMPT[max(end - CONTEXT_SIZE + 1, 1):end]
+prompt_vector = [UInt8(t) for t in prompt]
 
-        hvs = Vector{BitVector}(undef, SUBSAMPLES)
-        for i in 1:SUBSAMPLES
-            context = @view(con[rand(max(end - CONTEXT_SIZE + 1, 1):end):end])
-            # context = con
-            hvs[i] = gen_context_hvector(context)
-        end
-        hv = bundle(hvs)
-        push!(prompt_vector, predict(tm, TMInput(hv.chunks, hv.len)))
+for n in 1:TOKENS_GENERATE
+    con = @view(prompt_vector[max(end - CONTEXT_SIZE + 1, 1):end])
 
-        print(Char(prompt_vector[n + length(prompt)]))
+    hvs = Vector{BitVector}(undef, SUBSAMPLES)
+    for i in 1:SUBSAMPLES
+        context = @view(con[rand(max(end - CONTEXT_SIZE + 1, 1):end):end])
+        # context = con
+        hvs[i] = gen_context_hvector(context)
     end
-    println("\n")
+    hv = bundle(hvs)
+    push!(prompt_vector, predict(tm, TMInput(hv.chunks, hv.len)))
+
+    print(Char(prompt_vector[n + length(prompt)]))
 end
-
-
-sample()
+println("\n")
--- examples/TEXT/train.jl
+++ examples/TEXT/train.jl
@@ -39,7 +39,6 @@ for t in tokens
     println("$(Char(t)): $(get_stochastic_updates(tokens_probs[t]))")
 end
 
-
 if isfile(HV_PATH)
     hvectors = deserialize(HV_PATH)
 else
@@ -50,53 +49,47 @@ else
     serialize(HV_PATH, hvectors)
 end
 
-
 # Training the TM model
-function train()
-    hv_sample = gen_context_hvector(@view(CORPUS[1:CONTEXT_SIZE]))
-    x_sample = TMInput(hv_sample.chunks, hv_sample.len)
-    y_samples = collect(keys(hvectors))
-    tm = TMClassifier(x_sample, y_samples, CLAUSES, T, S, L=L, LF=LF, states_num=65536, include_limit=65000)
-    save(compile(tm), TM_PATH)  # Save empty model for sample()
+hv_sample = gen_context_hvector(@view(CORPUS[1:CONTEXT_SIZE]))
+x_sample = TMInput(hv_sample.chunks, hv_sample.len)
+y_samples = collect(keys(hvectors))
+tm = TMClassifier(x_sample, y_samples, CLAUSES, T, S, L=L, LF=LF, states_num=65536, include_limit=65000)
+save(compile(tm), TM_PATH)  # Save empty model for sample()
 
-    density = round(sum(x_sample) / length(x_sample) * 100, digits=2)
-    println("\nClasses: $(tm.classes_num), clauses: $(tm.clauses_num), T: $(tm.T), S: $(tm.S) (s: $(tm.s)), L: $(tm.L), LF: $(tm.LF), states_num: $(tm.state_max + 1), include_limit: $(tm.include_limit).")
-    println("Input vector size: $(length(x_sample)) bits, density: $(density)%, training dataset size: $(CORPUS_LENGTH).")
-    println("Expected average clause literal density: $(round(tm.L / length(x_sample) * 100, digits=2))%. Using literals index: false.")
-    println("Running in $(nthreads()) threads. Training over $(EPOCHS) epochs:\n")
-    all_time = @elapsed begin
-        for epoch in 1:EPOCHS
-            epoch_time = @elapsed begin
-                counter = Atomic{Int}(0)
-                @threads for _ in 1:SAMPLES_PER_EPOCH
-                    if counter[] > SAMPLES_PER_EPOCH
-                        break
-                    end
-                    start = rand(1:CORPUS_LENGTH - CONTEXT_SIZE - 1)
-                    finish = rand(start:start + CONTEXT_SIZE - 1)
-                    y = CORPUS[finish + 1]
-                    # Balancing classes
-                    @inbounds for i in 1:get_stochastic_updates(tokens_probs[y])
-                        context = @view(CORPUS[start:finish])
-                        hv = gen_context_hvector(context)
-                        x = TMInput(hv.chunks, hv.len)
-                        train!(tm, x, y)
-                        atomic_add!(counter, 1)
-                    end
+density = round(sum(x_sample) / length(x_sample) * 100, digits=2)
+println("\nClasses: $(tm.classes_num), clauses: $(tm.clauses_num), T: $(tm.T), S: $(tm.S) (s: $(tm.s)), L: $(tm.L), LF: $(tm.LF), states_num: $(tm.state_max + 1), include_limit: $(tm.include_limit).")
+println("Input vector size: $(length(x_sample)) bits, density: $(density)%, training dataset size: $(CORPUS_LENGTH).")
+println("Expected average clause literal density: $(round(tm.L / length(x_sample) * 100, digits=2))%. Using literals index: false.")
+println("Running in $(nthreads()) threads. Training over $(EPOCHS) epochs:\n")
+all_time = @elapsed begin
+    for epoch in 1:EPOCHS
+        epoch_time = @elapsed begin
+            counter = Atomic{Int}(0)
+            @threads for _ in 1:SAMPLES_PER_EPOCH
+                if counter[] > SAMPLES_PER_EPOCH
+                    break
+                end
+                start = rand(1:CORPUS_LENGTH - CONTEXT_SIZE - 1)
+                finish = rand(start:start + CONTEXT_SIZE - 1)
+                y = CORPUS[finish + 1]
+                # Balancing classes
+                @inbounds for i in 1:get_stochastic_updates(tokens_probs[y])
+                    context = @view(CORPUS[start:finish])
+                    hv = gen_context_hvector(context)
+                    x = TMInput(hv.chunks, hv.len)
+                    train!(tm, x, y)
+                    atomic_add!(counter, 1)
                 end
             end
-            epoch_time = Time(0) + Second(floor(Int, epoch_time))
-            println("Epoch #$(epoch) elapsed in $(epoch_time)")
-            save(compile(tm), TM_PATH)
         end
+        epoch_time = Time(0) + Second(floor(Int, epoch_time))
+        println("Epoch #$(epoch) elapsed in $(epoch_time)")
+        save(compile(tm), TM_PATH)
     end
-    elapsed = Time(0) + Second(floor(Int, all_time))
-    average_clause_density = round((literals_sum(tm) / (tm.classes_num * tm.clauses_num * 2)) / length(x_sample) * 100, digits=2)
-    println("\n$(EPOCHS) epochs done in $(elapsed).")
-    println("Classes: $(tm.classes_num), clauses: $(tm.clauses_num), T: $(tm.T), S: $(tm.S) (s: $(tm.s)), L: $(tm.L), LF: $(tm.LF), states_num: $(tm.state_max + 1), include_limit: $(tm.include_limit).")
-    println("Input vector size: $(length(x_sample)) bits, density: $(density)%, training dataset size: $(CORPUS_LENGTH).")
-    println("Average clause literal density: $(average_clause_density)%. Using literals index: false.\n")
 end
-
-
-train()
+elapsed = Time(0) + Second(floor(Int, all_time))
+average_clause_density = round((literals_sum(tm) / (tm.classes_num * tm.clauses_num * 2)) / length(x_sample) * 100, digits=2)
+println("\n$(EPOCHS) epochs done in $(elapsed).")
+println("Classes: $(tm.classes_num), clauses: $(tm.clauses_num), T: $(tm.T), S: $(tm.S) (s: $(tm.s)), L: $(tm.L), LF: $(tm.LF), states_num: $(tm.state_max + 1), include_limit: $(tm.include_limit).")
+println("Input vector size: $(length(x_sample)) bits, density: $(density)%, training dataset size: $(CORPUS_LENGTH).")
+println("Average clause literal density: $(average_clause_density)%. Using literals index: false.\n")
